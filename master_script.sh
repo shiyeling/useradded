3cf1dfeb606a2b969ba4371e3057edecd8ea6b21
Yeling Shi <shiyeling@gmail.com>
2015-04-29 23:42:02 +0800

    Add git related config command

--- master_script.sh
+++ master_script.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
 
+git config --global color.diff auto # git diff 要顯示顏色
+git config --global color.status auto # git status 要顯示顏色
+git config --global color.branch auto
+git config --global color.ui true
+git config --global core.editor vim
