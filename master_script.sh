fbdcf189a70fa7bd425bc4982cd8483416fbcfa4
Yeling Shi <yeling.shi@calix.com>
2015-05-15 09:33:42 +0800

    add more

--- master_script.sh
+++ master_script.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
 
+#Tools installation
+#install the vim-enhancement
+
+#Tools configuration
+#bash first, get the config entries from the resource/*.bash_profile file
+
+#git config
 git config --global color.diff auto # git diff 要顯示顏色
 git config --global color.status auto # git status 要顯示顏色
 git config --global color.branch auto
 git config --global color.ui true
 git config --global core.editor vim
+#follow github help page to generate private key and add public key to github account
