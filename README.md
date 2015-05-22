7ddbefbd3da9723429af2213ea64c65ab366531f
Yeling Shi <yeling.shi@calix.com>
2015-05-22 17:41:42 +0800

    add font install

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 scripts and configurations used to initialize new user account under Linux distro.
 Actually fedora for now.
 
+References:
+
+For the fedora system fonts: check below page out:
+http://fedoramagazine.org/5-great-monospaced-fonts-for-coding-and-the-terminal-in-fedora/
+
--- master_script.sh
+++ master_script.sh
@@ -6,6 +6,9 @@
 #Tools configuration
 #bash first, get the config entries from the resource/*.bash_profile file
 
+#System configuration
+#fonts:
+sudo yum install levien-inconsolata-fonts adobe-source-code-pro-fonts mozilla-fira-mono-fonts google-droid-sans-mono-fonts dejavu-sans-mono-fonts
 #git config
 git config --global color.diff auto # git diff 要顯示顏色
 git config --global color.status auto # git status 要顯示顏色
