8db4363a3a916c522a08916aaebc9ae3cde19e9b
Yeling Shi <yeling.shi@calix.com>
2015-06-16 10:17:00 +0800

    add git alias and also bash alis for git commands

--- master_script.sh
+++ master_script.sh
@@ -15,4 +15,9 @@ git config --global color.status auto # git status 要顯示顏色
 git config --global color.branch auto
 git config --global color.ui true
 git config --global core.editor vim
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.df diff
 #follow github help page to generate private key and add public key to github account
